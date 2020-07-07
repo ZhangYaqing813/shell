fbc4bf68ce9dc982749dd01563384ade20b81f7c
zhangyaqing <zhangyaqing59@126.com>
2020-07-07 19:53:13 +0800

    add jenkins shell change

--- test/test.sh
+++ test/test.sh
@@ -4,10 +4,32 @@ target_file=/data/test_rsync
 source_host=172.16.151.11
 data_backup=/tmp/test_rsync
 checksum=/data/checksum
-
+check_res=md5sum.md5
 #显示目标目录文件
 
-list_file(){
+checkfile() {
+    #备份文件是否完整
+    #$1 为校验结果存放的目录
+    #$2 校验结果文件
+    a=0
+    echo "checkfile 执行中======="
+    cd $1
+
+    echo "$1/$2 -----"
+    while read sum file
+    do
+        echo "while 执行"
+        flag=`sha256sum $file |awk '{print $1}'`
+        if [[ ${flag} == ${sum} ]]; then
+            echo " sucess"
+        else
+            echo " faild"
+        fi
+    done <$1/$2
+
+}
+
+list_file() {
     echo "###显示目标目录文件"
     rsync root@${source_host}:${source_file}/
     echo "<----------------------------->"
@@ -16,43 +38,40 @@ list_file(){
 
 }
 #同步前备份
-backup(){
+backup() {
     echo "执行备份----------"
-    #备份前生成检验文件
-
-    flag=`rsync -a  --backup --backup-dir=${data_backup}/ --suffix=~ ${target_file}/ ${data_backup}/`
     
-    #备份完后再次生成校验文件
-
-
-    echo "flag = ${flag}"
-    
-
-    if [[ "${flag}" == "" ]];then
-        echo "backup sucess"
-    
+    if [[ "$(ls -A ${data_backup})"="" ]]; then
+        
+        echo ">>>>>>>>>>>>>>>>>>"
+        rm -rf ${data_backup}/*
+        sudo sha256sum ${target_file}/* >${data_backup}/${check_res}
+        flag=$(rsync -a --backup --backup-dir=${data_backup}/ ${target_file}/ ${data_backup}/)
+        checkfile ${data_backup} ${check_res}
+    else
+        echo "<<<<<<<<<<<<<<<<<<<<<<<<"
+        flag=$(rsync -a --backup --backup-dir=${data_backup}/ ${target_file}/ ${data_backup}/)
+        #备份完后再次校验备份文件
+        checkfile ${data_backup} ${check_res}
     fi
-
 }
 #执行同步
-brsync(){
-    echo "开始同步--------"
-    ssh root@${source_host} "md5sum ${source_file} >${checksum}/check.md5"
-    rsync -a -e "ssh -p 22" root@${source_host}:${source_file}/ $target_file/
-    
-}
 
+brsync() {
+    echo "开始同步--------"
+    ssh root@${source_host} "md5sum ${source_file}/* >${source_file}/${check_res}"
+    rsync -a -e "ssh -p 22" root@${source_host}:${source_file}/ $target_file/
+    checkfile ${target_file} ${check_res}
+
+}
 
 #文件校验，如果校验失败退出，并还原
 
-list_file
-sleep 5
+#list_file
+#sleep 5
+
 
 backup
 sleep 5
-
 brsync
-
-
-
-
+#brsync
